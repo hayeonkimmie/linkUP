f3fe3f7f8fa2917a27d2284b2b900428b17a512c
hayeon <hayeonkimmie@gmail.com>
2025-04-29 10:27:01 +0900

    updates : PaymentInfo

--- src/main/java/controller/client/ClientPaymentInfo.java
+++ src/main/java/controller/client/ClientPaymentInfo.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 @WebServlet("/paymentInfo")
 public class ClientPaymentInfo extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
     public ClientPaymentInfo() {
         super();
     }
